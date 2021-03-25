75fd5bb47afd03c5cca6fa14d35a1baee582a35b
Alexander Thompson <alexthompson429@gmail.com>
2021-03-24 23:06:37 -0600

    front page

---
+++ client/src/components/front page/homepage.js
@@ -0,0 +1,11 @@
+<Card className="text-center">
+    <Card.Header>Games</Card.Header>
+    <Card.Body>
+      <Card.Title>Special title treatment</Card.Title>
+      <Card.Text>
+    
+      </Card.Text>
+      <Button variant="primary">Go somewhere</Button>
+    </Card.Body>
+    <Card.Footer className="text-muted">2 days ago</Card.Footer>
+  </Card>

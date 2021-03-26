60bb9b3d2383c3df36e08769c6874d520ad7d86b
Alexander Thompson <alexthompson429@gmail.com>
2021-03-26 14:08:00 -0600

    new user page

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -5,17 +5,6 @@ function Nav() {
     <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
       <a className="navbar-brand" href="/">
         MyGameLibrary
-<<<<<<< HEAD
-        <div class = "navbar navbar-dark bg-dark" role = "toolbar" >
-        <div class="col text-center">
-        <div class = "btn-group"  >
-           <button type = "button" class = "btn btn-default" style="background-color: purple;">Search</button>
-           <button type = "button" class = "btn btn-default" style="background-color: purple;">My games</button>
-           <button type = "button" class = "btn btn-default" style="background-color: purple;">Account</button>
-        </div>
-        </div>
-     </div>
-=======
 
         <div class = "navbar navbar-dark bg-dark" role = "toolbar" >
         <div class="col text-center">
@@ -26,7 +15,6 @@ function Nav() {
         </div>
         </div>
        </div>
->>>>>>> c601d763abb543918d704b65be04a95fb058031f
       </a>
     </nav>
   );

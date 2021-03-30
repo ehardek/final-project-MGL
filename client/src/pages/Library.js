f2c6f3d096f8064e4da18d42c5d211ab0269e2e4
Evan Hardek <evanhardek@gmail.com>
2021-03-30 00:38:35 -0400

    fixed card component

--- client/src/pages/Library.js
+++ client/src/pages/Library.js
@@ -1,37 +1,21 @@
 import React from "react";
-import "./Box.css";
 import {Card} from "react-bootstrap";
 
 const MyGameLibrary = () => {
 
 const renderCard = (car,index) =>{
   return(
-    Card style={{ width: '18rem' }}>
+    <Card style={{ width: '18rem' }}>
   <Card.Img variant="top" src="holder.js/100px180" />
   <Card.Body>
-    <Card.Title>Card Title</Card.Title>
+    <Card.Title>Game Title</Card.Title>
     <Card.Text>
-      Some quick example text to build on the card title and make up the bulk of
-      the card's content.
+      Game Description
     </Card.Text>
   </Card.Body>
 </Card>
 );
 };
-return (<div classname = "App">;
-<Card style={{ width: '18rem' }}>
-<Card.Img variant="top" src="holder.js/100px180" />
-<Card.Body>
-<Card.Title>Card Title</Card.Title>
-<Card.Text>
-  Some quick example text to build on the card title and make up the bulk of
-  the card's content.
-</Card.Text>
-</Card.Body>
-</Card>
-</div>
-)
-
 };
 
    

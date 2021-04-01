ef58d0915f4004ae610736f29761485fff243419
Alexander Thompson <alexthompson429@gmail.com>
2021-03-31 20:02:55 -0600

    Added css to library with var

--- client/src/pages/Library.js
+++ client/src/pages/Library.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Box.css";
 import {Card} from "react-bootstrap";
+import { Body } from "react-bootstrap/lib/Media";
 
 const MyGameLibrary = () => {
 const Games = [
@@ -13,12 +14,11 @@ const Games = [
 const renderCard = (car,index) =>{
   return(
     <Card style={{ width: '18rem' }}>
-  <Card.Img variant="top" src="holder.js/100px180" />
+  <Card.Img variant="top" src= {Card.Image} />
   <Card.Body>
-    <Card.Title>Card Title</Card.Title>
+    <Card.Title>{Card.title}</Card.Title>
     <Card.Text>
-      Some quick example text to build on the card title and make up the bulk of
-      the card's content.
+      {Card.title}
     </Card.Text>
   </Card.Body>
 </Card>
@@ -43,3 +43,17 @@ return (<div classname = "App">;
    
 export default MyGameLibrary
 
+var styles = `
+    .qwebirc-qui .ircwindow div { 
+        font-family: Georgia,Cambria,"Times New Roman",Times,serif;
+        margin: 26px auto 0 auto;
+        max-width: 650px;
+    }
+    .qwebirc-qui .lines {
+
+`
+
+var styleSheet = document.createElement("style")
+styleSheet.type = "text/css"
+styleSheet.innerText = styles
+document.head.appendChild(styleSheet)
